059ea4be461e325dc03d3f1021250b128cc1f7e3
rafaelgarcia-dev <rafa48429@gmail.com>
2024-02-21 22:54:12 -0300

    Alteração feita em Fale conosco

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
 const form = document.querySelector('.Formulario')
 const mascara = document.querySelector('.mascara-formulario')
-
+const faleconosco = document.querySelector('.faleconosco')
 
 function cliqueiNoBotao(){
     form.style.transform= 'translateX(-50%)'
     form.style.left='50%'
     mascara.style.visibility= 'visible'
+    faleconosco.style.color= 'black'
 }
 
 function cliqueiNoBotao1(){
     form.style.top='30%'
     form.style.left='-290%'
     mascara.style.visibility= 'hidden'
+    faleconosco.style.color= 'white'
 }
